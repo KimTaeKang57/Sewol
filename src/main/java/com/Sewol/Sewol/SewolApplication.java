2489562a946800b2c1a71be6582ff6d99e194234
김어진 <qlql12@kyonggi.ac.kr>
2021-09-28 17:25:06 +0900

    start

---
+++ src/main/java/com/Sewol/Sewol/SewolApplication.java
@@ -0,0 +1,13 @@
+package com.Sewol.Sewol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SewolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SewolApplication.class, args);
+	}
+
+}
